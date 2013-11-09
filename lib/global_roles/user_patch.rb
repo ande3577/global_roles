71e70cdd2690691ebda6233fa81e4868f8c60976
ande3577 <ande3577@gmail.com>
2013-11-09 10:05:12 -0600

    Added support for managing builtin redmine permissions

--- lib/global_roles/user_patch.rb
+++ lib/global_roles/user_patch.rb
@@ -7,6 +7,8 @@ module GlobalRoles
       base.class_eval do
         has_many :x_roles, :class_name => 'GlobalRole', :foreign_key => 'user_id' #, :include => :roles
         has_many :global_roles, :source => :role, :through => :x_roles  
+        alias_method_chain :roles_for_project, :global_roles
+        alias_method_chain :allowed_to?, :global_roles
       end
     end
 
@@ -25,6 +27,31 @@ module GlobalRoles
           r.allowed_to?({:controller => controller, :action => action}) 
         }.nil?
      
+      end
+
+      def roles_for_project_with_global_roles(project)
+        add_global_roles(roles_for_project_without_global_roles(project), project)
+      end
+
+      def add_global_roles(roles, project)
+        roles = (roles + self.global_roles).uniq if should_add_global_roles?(project)
+        roles
+      end
+
+      def should_add_global_roles?(project)
+        project && project.is_public? && !project.archived?
+      end
+
+      def allowed_to_with_global_roles?(action, context, options = {}, &block)
+        return true if allowed_to_without_global_roles?(action, context, options, &block)
+        if context.nil? && options[:global]
+          self.global_roles.any? {|role|
+            role.allowed_to?(action) &&
+            (block_given? ? yield(role, self) : true)
+          }
+        else
+          false
+        end
       end
 
     end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,13 +1,70 @@
 require File.expand_path('../../test_helper', __FILE__)
 
 class UserTest < ActiveSupport::TestCase
-  fixtures :projects, :users, :members, :member_roles, :roles, :enabled_modules
+  fixtures :projects, :users, :roles, :enabled_modules
   
   def setup
+  	@admin = User.where(:admin => true).first
+  	@user = User.logged.where(:admin => false).first
+  	@project = Project.active.where(:is_public => true).first
+  	@project_role = @user.roles_for_project(@project).first
+  	@global_role = Role.find(2)
+  	@x_role = GlobalRole.create!(:role => @global_role, :principal => @user)
+  	@user.reload
   end
-  
-  def test_add_global_role
-  	pending
+
+  def test_global_role_added_to_roles_for_project
+  	assert @user.roles_for_project(@project).include?(@global_role)
+  end
+
+  def test_global_role_still_includes_project_roles
+  	assert @user.roles_for_project(@project).include?(@project_role)
+  end
+
+  def test_global_role_not_added_to_private_project
+  	make_project_private
+  	assert !@user.roles_for_project(@project).include?(@global_role)
+  end
+
+  def test_private_project_still_includes_project_roles
+  	make_project_private
+  	assert @user.roles_for_project(@project).include?(@project_role)
+  end
+
+  def test_global_role_not_added_to_archived_project
+  	@project.archive
+  	assert @user.roles_for_project(@project).empty?
+  end
+
+  def test_allowed_globally_returns_true_for_global_role
+    destroy_project_membership
+    add_global_permission
+
+    assert @user.allowed_to?(:add_project, nil, :global => true)
+  end
+
+  def test_allowed_globally_returns_false_if_not_have_global_role
+  	destroy_project_membership
+    assert !@user.allowed_to?(:add_project, nil, :global => true)
+  end
+
+  def test_existing_global_permissons_still_observed
+  	assert @user.allowed_to?(:add_project, nil, :global => true)
+  end
+
+  private
+  def make_project_private
+  	@project.update_attribute(:is_public, false)
+  	@project.reload
+  end
+
+  def destroy_project_membership
+    @user.memberships.destroy_all
+    @user.reload
+  end
+
+  def add_global_permission
+    @global_role.add_permission!(:add_project)
   end
   
 end
