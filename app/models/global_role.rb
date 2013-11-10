2ea615d5c25f0cdfa92b92c3945c655c805c1a68
ande3577 <ande3577@gmail.com>
2013-11-10 09:13:36 -0600

    Validates principal, role

--- app/models/global_role.rb
+++ app/models/global_role.rb
@@ -2,6 +2,8 @@ class GlobalRole < ActiveRecord::Base
   unloadable
 
   belongs_to :role
+  validates_presence_of :role
   belongs_to :principal, :foreign_key => :user_id
+  validates_presence_of :principal
 
 end
